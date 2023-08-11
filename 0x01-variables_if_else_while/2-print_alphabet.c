f5bc8dcd96e30d572a5863fd67554421fae06962
rumonah <rummaloba@gmail.com>
2023-08-11 14:32:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	 i = 'a';
+	 while 
+	      (i <= 'z') {
+		      putchar(i);
+		      c++
+	      }
+	 putchar("\n");
+	 return (0);
+}
+
+

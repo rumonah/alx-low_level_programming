e67eb7f6af83a762c8e7432200d8875807aeda52
rumonah <rummaloba@gmail.com>
2023-08-11 15:04:11 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 /**
  * main - function main
- * 
+ *
  * Return: always 0
  */
+
 int main(void)
 {
 	char i;
-
-	 i = 'a';
-	 while 
-	      (i <= 'z') {
-		      putchar(i);
-		      i++
-	      }
-	 putchar("\n");
-	 return (0);
-}
-
-
+	
+	i = 'a';
+		while
+			(i <= 'z') {
+				putchar(i);
+				i++
+			}
+		putchar("\n);
+		return (0);

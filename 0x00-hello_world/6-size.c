a8b74e21e18d8045160416f2f19d56f7e8fb9436
rumonah <rummaloba@gmail.com>
2023-08-10 19:44:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 {        
 printf("Size of a char: 1 byte(s));
 printf("Size of a int: 4 byte(s));
-printf("Size of a long int: 4 byte(s);)
-printf("Size of a long long int: 8 byte(s);)	
+printf("Size of a long int: 4 byte(s))
+printf("Size of a long long int: 8 byte(s))	
 printf("Size of a float: 4 byte(s));
 return (0);
 }

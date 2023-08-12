09ce3e2e7cecdc7e0ba64c10e37d041a19bc8f58
rumonah <rummaloba@gmail.com>
2023-08-11 17:07:27 -0700

    3 alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char r;
+	char j;
+	r = 'a';
+	j = 'A';
+	while
+		(r <= 'z') 
+		{
+			putchar(r);
+			r++;
+		}
+	while
+	       	(j <= 'Z')
+		{
+		putchar(j);
+		j++;
+		}
+	putchar("\n");
+	return (0);
+}

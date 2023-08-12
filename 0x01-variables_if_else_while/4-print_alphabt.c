dcabfe59346abe439a51062a3dd424a1e529b171
rumonah <rummaloba@gmail.com>
2023-08-12 08:06:14 -0700

    some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print some alphabet letters
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char r;
+
+	for(r = 'a';r <= 'z';r++);
+	{
+	if(r! = 'e' && c! ='q');
+	{
+		putchar(r);
+	}
+	}
+	putchar('\n');
+
+	return (0);

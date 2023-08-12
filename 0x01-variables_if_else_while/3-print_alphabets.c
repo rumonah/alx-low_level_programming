d9025510795729a5d6c45355771a85fbd74f1270
rumonah <rummaloba@gmail.com>
2023-08-12 13:50:04 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,13 @@
 
 int main(void)
 {
-	char alpha[52]=("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYZ");
+	char alpha[52] = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYZ");
 	int r;
 
-	if (r = 0;r < 52;r++)
+	for (r = 0 ; r < 52 ; r++)
 	{
 		putchar(alpha)(r);
 	}
-	
 	putchar('\n');
 
 	return (0);

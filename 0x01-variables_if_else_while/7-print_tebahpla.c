4d9318e78699444b0df0f4547245675ddd692d6f
rumonah <rummaloba@gmail.com>
2023-08-11 19:45:16 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main -lowercase alphabets in reverse
+* new line is to follow
+* Return: always 0
+*/
+int main(void)
+{
+	char ru;
+
+	for (ru = 'z';ru >= 'a'; ru--)
+	{
+		putchar(ru);
+	}
+	putchar("\n);
+	return (0);

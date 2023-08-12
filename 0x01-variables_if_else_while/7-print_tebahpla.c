4b0db9fa898b283d285b688a87596626bcada64d
rumonah <rummaloba@gmail.com>
2023-08-12 15:01:33 -0700

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 /**
 * main -lowercase alphabets in reverse
-* new line is to follow
+* 
 * Return: always 0
 */
+
 int main(void)
 {
 	char ru;
-
-	for (ru = 'z';ru >= 'a'; ru--)
-	{
+	
+	for (ru = 'z' ; ru >= 'a' ; ru--)
 		putchar(ru);
-	}
-	putchar("\n);
+	putchar('\n');
 	return (0);
+}

c2663381931f6ccefe84cb4f08d85e0c9a15803a
rumonah <rummaloba@gmail.com>
2023-08-13 08:20:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return 1 (Success)
- *
- */
+* main - Entry point
+*
+* Return: 1 (Success)
+*/
+
 int main(void)
-{	
-fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",stdout);
-return (1);
+{
+	fput("and that piece of art is useful\" -  Dora Korpar, 2015 - 10 - 19\n", stdout);
+	return (1);
 }

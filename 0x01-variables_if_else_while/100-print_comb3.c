700d9a643b981e9701a3c78589ce36ea944d4d37
rumonah <rummaloba@gmail.com>
2023-08-12 18:24:45 -0700

    comb4

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,10 +8,11 @@
 
 int main(void)
 {
-	int r,j;
-	for (r = '0' ; j <= '9' ; r++)
-	{	
-		for (j = '0'; r <+ '9' ; j++)
+	int r, j;
+
+	for (r = '0' ; j < = '9' ; r++)
+	{
+		for (j = '0' ; r  <+ '9' ; j++)
 		{
 			if (r < j)
 			{

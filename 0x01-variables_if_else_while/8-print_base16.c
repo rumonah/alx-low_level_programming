18ea756c33a7b778a56c05a70e71b5b0e075519b
rumonah <rummaloba@gmail.com>
2023-08-14 11:25:50 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	int r;
-	
-	for (r = 48 ; r <= 58 ; r++)
+
+	for (r = 48 ; r < 58 ; r++)
 		putchar(r);
-	for (r = 97 ; r <= 103 ; r++)
+	for (r = 97 ; r < 103 ; r++)
 		putchar(r);
 	putchar('\n');
 	return (0);

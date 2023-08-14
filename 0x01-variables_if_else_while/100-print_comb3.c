f3a906ed156371dfd096c9f6069df712951830bd
rumonah <rummaloba@gmail.com>
2023-08-14 11:31:35 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int r, j;
 
-	for (r = '0' ; j <= '9' ; r++)
+	for (r = 0 ; j <= 9 ; r++)
 	{
-		for (j = '0' ; r <= '9' ; j++)
+		for (j = 0 ; r <= 9 ; j++)
 		{
 			if (r < j)
 			{
 				putchar(r);
 				putchar(j);
-				if (r != '9' || (r == '9' && j != '9'))
+				if (r != 9 || (r == 9 && j != 9))
 				{
 					putchar(',');
 					putchar(' ');

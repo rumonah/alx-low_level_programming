46a3451915c80145ea47a787f12a03229bbd5b18
rumonah <rummaloba@gmail.com>
2023-08-14 10:29:56 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,8 @@ int main(void)
 	printf("Size of an int : %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int : %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int : %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float : %lu byte(s)\n", (unsigned long)sizeof(e);
+	printf("Size of a float : %lu byte(s)\n", (unsigned long)sizeof(e));
+	
 	return (0);
 }
+

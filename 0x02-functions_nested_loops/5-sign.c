f4cc1a0e37b84cf69bcce8d8cff63c2ce2b347fa
rumonah <rummaloba@gmail.com>
2023-08-15 11:27:20 -0700

    What is the ASCII value of a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+include "main.h"
+/**
+ * print_sign -print the sign of a number
+ * @n: the numbers to be cheched
+ * Return: 1 for positive number, -1 for negative numbers or 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar(46);
+		return (-1);
+	}
+	else 
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+

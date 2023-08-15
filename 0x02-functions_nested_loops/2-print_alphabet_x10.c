2388c6013c46063aece8e6f016bee11275d010c1
rumonah <rummaloba@gmail.com>
2023-08-15 10:49:33 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,16 +9,13 @@ void print_alphabet_x10(void)
 {
 	int r;
 	char j;
-
-	for (r = 9 ; r < 9) 
+	r = 0;
+	while (r < 10)
+	
+	for (j = 'a'; j <= 'z' ; j++) 
 	{
-		_putchar(r++)
+		_putchar(j);
 	}
-	{
-		for (j = 'a' ; j <= 'z')
-		{
-			_putchar(j++);
-		}
-	}
-	return (0);
+	_putchar('\n');
+	r++;
 }

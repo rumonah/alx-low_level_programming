773e405f4d99e284a9825e489fb1bce34a27daa7
rumonah <rummaloba@gmail.com>
2023-08-16 00:44:39 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - print the time
+ *
+ * Description: print time in min for a 24 hours clock
+ *
+ */
+void jack_bauer(void)
+{
+	int M, H;
+
+	for (H =0 ; H < 24 ; H++)
+	{
+		for (M = 0 ; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) +'0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

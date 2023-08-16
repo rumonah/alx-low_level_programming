693f68e3a9b775b12b7f9cc5a63cf2dc42c161b3
rumonah <rummaloba@gmail.com>
2023-08-16 00:47:13 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,14 +9,14 @@ void jack_bauer(void)
 {
 	int M, H;
 
-	for (H =0 ; H < 24 ; H++)
+	for (H = 0 ; H < 24 ; H++)
 	{
 		for (M = 0 ; M < 60; M++)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
 			_putchar(':');
-			_putchar((M / 10) +'0');
+			_putchar((M / 10) + '0');
 			_putchar((M % 10) + '0');
 			_putchar('\n');
 		}

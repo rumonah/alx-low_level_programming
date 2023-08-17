b2d45d512323c9fdc67ab9dd3b1b049010d39fb8
rumonah <rummaloba@gmail.com>
2023-08-17 10:48:48 -0700

    largest

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,11 +11,11 @@ int main(void)
 	int a, b, c;
 	int largest;
 	
-	a = 874;
-	b = 98;
+	a = 972;
+	b = -98;
 	c = 0;
 	
-	largest = ( a + b + c);
+	largest = (a - b + c);
 	
 	printf("%d is the largest number\n", largest);
 	

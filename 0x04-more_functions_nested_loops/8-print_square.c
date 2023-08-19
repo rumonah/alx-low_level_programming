e793b89a94ba62b5211eea0e03c3a96cf22641e5
rumonah <rummaloba@gmail.com>
2023-08-19 09:36:00 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ void print_square(int size)
 	
 	if (size <= 0)
 		_putchar('\n');
-	for (a = 0 ; a < (size) ; a++)
+	for (a = 1 ; a <= (size) ; a++)
 	{
-		for (b = 0 ; b < (size) ; b++)
+		for (b = 1 ; b <= (size) ; b++)
 		{
 			_putchar('#');
 		}

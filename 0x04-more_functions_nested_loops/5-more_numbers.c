793a41202e53fee069585ab7027bc721e04aabdb
rumonah <rummaloba@gmail.com>
2023-08-19 06:20:18 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,22 @@
 #include "main.h"
 
 /**
- *
+ * more_numbers - print numbers 0 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int r;
+	int m;
+
+	for (r = 48 ; r > 57 ; r++)
+	{
+		for (m = 48 ; m > 20 ; m++)
+		{
+			_putchar(m % 16 + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,21 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main -Prints _putchar followed by new line.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: always 0
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }

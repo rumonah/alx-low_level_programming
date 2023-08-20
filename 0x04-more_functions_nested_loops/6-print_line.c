f8d61fcf02c702e62fae673db4dedcdccec525e2
rumonah <rummaloba@gmail.com>
2023-08-20 15:18:13 -0700

    loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,21 +3,17 @@
 /**
  * print_line - print straight lines on terminal
  * @n: number of times character '_' will be printed
- * Return: void
+ * Return: 0
  */
 
 void print_line(int n)
 {
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
 		int r;
 
-		for (r = 1 ; r < n ; r++)
+		for (r = 0 ; r < n ; r++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
-	}
+
 }

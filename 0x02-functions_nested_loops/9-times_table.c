144bb2b0ab2ed61e3c327967c9e87aed12e0070d
rumonah <rummaloba@gmail.com>
2023-08-20 19:03:03 -0700

    times 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,30 +12,30 @@ void times_table(void)
 {
 	int j, k, m;
 
-	for (j = 0 ; j <= 9 ; j++)
+	for (j = 1 ; j <= 9 ; j++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-	}
-	for (k = 0 ; k <= 9 ; k++)
-	{
-		m = j * k;
-
-		if ((m / 10) > 0)
-		{
-			_putchar((m / 10) + '0');
-		}
-		else
-		{
-			_putchar(' ');
-		}
-		_putchar((m % 10) + '0');
-		if 
 		
+		for (k = 1 ; k <= 9 ; k++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			m = j * k;
+			
+			if ((m / 10) > 0)
+			{
+				_putchar((m / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((m % 10) + '0');
+			if (k < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}

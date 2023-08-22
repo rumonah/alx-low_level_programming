bfac8d288325ef362cebd5c20078c3ae3f11150a
rumonah <rummaloba@gmail.com>
2023-08-21 19:02:47 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,10 @@ void puts_half(char *str)
 {
 	int a, n, numb = 0;
 
-	for (a = 0 : str[a] != '\0' ; a++)
-		numb++
-			n = (numb - 1) / 2;
+	for (a = 0 ; str[a] != '\0' ; a++)
+		numb++;
+	n = (numb - 1) / 2;
 	for (a = n + 1 ; str[a] != '\0' ; a++)
 		_putchar(str[a]);
-	_putchar('\n')
+	_putchar('\n');
 }

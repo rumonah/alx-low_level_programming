8b062f989b4aa5a78267b7e222357bec703a52c0
rumonah <rummaloba@gmail.com>
2023-08-25 06:40:57 -0700

    print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: integer parameter
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n2;
+
+	n2 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n2 = -n;
+	}
+	if (n2 / 10 != 0)
+	{
+		print_number(n2 /10);
+	}
+	_putchar((n2 % 10) + '0');
+}

48f7f4a6636bd9f9df6edade986331af2aa8db6c
rumonah <rummaloba@gmail.com>
2023-08-26 22:15:45 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 /**
  * print_alphabet - prints the alphabets
+ *
  * Return: void
  */
 
 void print_alphabet(void)
 {
 	char r;
-	
+
 	for (r = 'a' ; r <= 'z'; r++)
 		_putchar(r);
 	_putchar('\n');
 }
-

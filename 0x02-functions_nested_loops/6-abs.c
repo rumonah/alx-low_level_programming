b253ca0da01555a926a3d3a64d41ec6130380784
rumonah <rummaloba@gmail.com>
2023-08-26 22:29:00 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,16 @@
 
 /**
  * _abs -computes the absolute value of an integer
- * @r: the number to be computed
+ * @c: the number to be computed
  * Return: absolute value of number or 0
  */
-int _abs(int c)
+int _
+abs(int c)
 {
 	if (c < 0)
 	{
 		int abs_val;
+
 		abs_val = c * -1;
 		return (abs_val);
 	}

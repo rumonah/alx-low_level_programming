e5f3a84a505d26d38c325771acdec33d1943c9a8
rumonah <rummaloba@gmail.com>
2023-08-30 09:56:47 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts_recursion - prints a string
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}

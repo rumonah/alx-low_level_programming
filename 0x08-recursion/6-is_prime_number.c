ce06ff4320e103d00a581453caf263a58b5f0a8d
rumonah <rummaloba@gmail.com>
2023-08-31 10:57:58 -0700

    6-prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _prime_number - function that returns 1 inputs integer
+ *@n: integer to be used
+ * Return: void
+ */
+
+int is_prime_number(int n)
+{
+	if (n > 1)
+		return (1);
+	if (n == 0)
+		return (0);
+
+}

62735e59c0f09fe3b94b1497ad4459d1c32659e3
rumonah <rummaloba@gmail.com>
2023-09-03 09:10:32 -0700

    2-largest_number.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -10,14 +10,13 @@ int main(void)
 {
 	int a, b, c;
 	int largest;
-	
+
 	a = 972;
 	b = -98;
 	c = 0;
-	
 	largest = (a - b + c);
-	
+
 	printf("%d is the largest number\n", largest);
-	
+
 	return (0);
 }

c09785d064631d75c4c7cb2c0a5da68bffba4255
rumonah <rummaloba@gmail.com>
2023-09-03 10:49:48 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - set the value of poineters to a character
+ * @s: pointer
+ * @to: character
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

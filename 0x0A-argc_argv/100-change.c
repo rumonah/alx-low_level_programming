5553bcb5b6ff9ae1bd7e0c988b0b20ec560b4e8d
rumonah <rummaloba@gmail.com>
2023-09-03 05:38:48 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,13 +24,13 @@ int main(int argc, char *argv[])
 	{
 		if (cents >= 25)
 			cents -= 25;
-		if (cents >= 10)
+		else if (cents >= 10)
 			cents -= 10;
-		if (cents >= 5)
+		else if (cents >= 5)
 			cents -= 5;
-		if (cents >= 2)
+		else if (cents >= 2)
 			cents -= 2;
-		if (cents >= 1)
+		else if (cents >= 1)
 			cents -= 1;
 		coind += 1;
 	}

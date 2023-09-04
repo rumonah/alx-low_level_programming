c3ad28e1b4c6f5441648093788e9b5174989dcde
rumonah <rummaloba@gmail.com>
2023-09-04 13:08:59 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,7 +33,7 @@ char *str_concat(char *s1, char *s2)
 	for (r = 0 ; s1[r] != '\0' ; r++)
 		output[r] = s1[r];
 	for (r = 0 ; s2[r] != '\0' ; r++)
-		output[s1size] = s2[r];
+		output[s1size + r] = s2[r];
 	return (output);
 
 }

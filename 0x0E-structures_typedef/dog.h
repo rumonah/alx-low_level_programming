f73f7ade532f8c31cfc47bd580ba7e9c4efa4d62
rumonah <rummaloba@gmail.com>
2023-09-11 10:48:14 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ struct dog
 	char *owner;
 };
 
-void int_dog(struct dog *d, char *name, float age. char *owner);
+void init_dog(struct dog *d, char *name, float age. char *owner);
 void print_dog(struct dog *d);
 typeof struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);

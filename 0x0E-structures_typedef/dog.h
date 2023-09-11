790494667278c7eebe3f606c7ee74790c036fc98
rumonah <rummaloba@gmail.com>
2023-09-11 10:42:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic inf on dog's
+ * @name: the name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void int_dog(struct dog *d, char *name, float age. char *owner);
+void print_dog(struct dog *d);
+typeof struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
+
+#endif

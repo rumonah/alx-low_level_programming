8ac001d4153e644b2fe15f51ad1167467ccca31e
rumonah <rummaloba@gmail.com>
2023-09-12 08:14:41 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,10 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
 /**
  * new_dog - create a new dog
  * @name: dog's name
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Retun: NULL if function fails
+ * Return: pointer to new dog success, NULL otherwise
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -25,7 +28,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (my2 + 1));
-	if (dog - >owner == NULL)
+	if (dog->owner == NULL)
 	{
 		free(dog);
 		free(dog->name);
@@ -34,4 +37,53 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
+}
+
+/**
+ * _strlen - return string length
+ * @s: string to evaluate
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int new;
+
+	new = 0;
+
+	while (s[new] != '\0')
+	{
+		new++;
+	}
+
+	return (new);
+}
+
+/**
+ **_strcpy - copies string pointer to by src include
+ * the terminationing null bytes (\0) to the buffer pointer
+ * @dest: pointer to the buffer in which  we copy the string
+ * @src: string to be copied
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int mynew, r;
+
+	mynew = 0;
+
+	while (src[mynew] != '\0')
+	{
+		mynew++;
+	}
+
+	for (r = 0 ; r < mynew ; r++)
+	{
+		dest[r] = src[r];
+	}
+	dest[r] = '\0';
+
+	return (dest);
 }

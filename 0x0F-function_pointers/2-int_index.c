131d016999644c78514d621b481759928a402d43
rumonah <rummaloba@gmail.com>
2023-09-14 01:28:43 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,21 +2,24 @@
 #include "function_pointers.h"
 
 /**
- * int-index -  function that searches for an integer
+ * int_index - function that searches for an integer
+ * @array: array
  * @cmp: pointer to the function to be used to compare values
- * @size: number of elements in the array 
+ * @size: size of elements in the array
  * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int r;
 
-        if (array == NULL || size <= 0 || cmp == NULL)
-                return (-1);
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
 	for (r = 0 ; r < size ; r++)
 	{
-                if (cmp(array[r]))
-                        return (r);
-        }
+		if (cmp(array[r]))
+			return (r);
+	}
+
 	return (-1);
 }
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * main -  performs simple operations.
+ * @argv: array of pointers
+ * @argc: argument count
+ * Return: 0
+ */
+
+int main(int __attribute__((__unused__))argc, char *argv[])
+{
+	int count1, count 2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	count1 = atoi(argv[1]);
+	op = argv[2];
+	count2 = atoi(argv[3]);
+
+	if ((*op == '/' && count2 == 0) || (*op == '%' && count == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n" get_op_fun(op)(count1 count2));
+
+	return (0);
+}

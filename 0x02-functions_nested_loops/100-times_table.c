138cbad8f7bdd9600b9d7921007bd74749e0b523
rumonah <rummaloba@gmail.com>
2023-10-19 05:15:41 -0700

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,47 +9,40 @@ void print_times_table(int n)
 {
 	int multi, digit, result;
 
-	if (n >= 0 && n <= 15)
+	if (!(n > 15 || n < 0))
 	{
-		for (digit = 0 ; digit <= n ; digit++)
+		for (multii = 0; multi <= n; multi++)
 		{
-			_putchar('0');
-			for (multi = 1 ; multi <= n; multi++)
+			for (digit = 0; digit <= n; digit++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				result = digit * multi;
-					if (digit == 0)
-					{
-						_putchar(result + '0');
-					}
-					else if (result < 10 && digit != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(result + '0');
-					}
-					else if (result >= 10 && result < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((result / 10) + '0');
-						_putchar((result % 10) + '0');
-					}
-					else if (result >= 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((result / 100) + '0');
-						_putchar(((result / 10) % 10) + '0');
-						_putchar((result % 10) + '0');
-					}
+				res = (multi * digit);
+				if (digit != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
+				if (result < 10 && digit != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 10 && result < 100)
+				{
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 100 && digit != 0)
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+					_putchar((result % 10) + '0');
 			}
+			_putchar('\n');
 		}
-	_putchar('\n');
+	}
 }

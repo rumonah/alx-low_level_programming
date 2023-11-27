8e9d4e85f8db7a0291393d8d4a8debe9befd2552
rumonah <rummaloba@gmail.com>
2023-11-27 10:51:54 -0800

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,12 +26,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (new = 0 ; text_content[new];)
 			new++;
+		r = write(file, text_content, new);
+
+		if (r == -1)
+			return (-1);
 	}
-	r = write(file, text_content, new);
-
-	if (r == -1)
-		return (-1);
-
 	close(file);
 
 	return (1);

66bbb1779e6cfb467f22751bc016c5ee081af100
rumonah <rummaloba@gmail.com>
2023-11-30 23:43:28 -0800

    correct

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 			txt++;
 	}
 
-	file = open(filename, O_CREAT | O_RDWR | O_APPEND, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	r = write(file, text_content, txt);
 
 	if (file == -1 || r == -1)

34527142374fe3450af492c73540946636bcf777
rumonah <rummaloba@gmail.com>
2023-11-30 23:37:19 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/*
+/**
  * create file - function that creates a file.
  * @filename: name of the file to be created
- * @text_content:  is NULL create an empty file
+ * @text_content:  is NULL terminated string
+ * to write to the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
@@ -25,5 +26,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(file);
-	return (1);
+	return (-1);
 }

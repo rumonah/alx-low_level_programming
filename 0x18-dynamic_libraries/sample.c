3607255df193582aff90e37ba41907ba3ae272f3
rumonah <rummaloba@gmail.com>
2024-01-19 07:32:09 -0800

    jackpot

--- 0x18-dynamic_libraries/sample.c
+++ 0x18-dynamic_libraries/sample.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * printf - prints the jackpot program
+ * @format - nothing
+ * Return: success
+ */
 int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 + 9\n", 17);
